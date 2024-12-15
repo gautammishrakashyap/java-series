d2279415021e0bb0dd11764f7e3b0d96c1f16900
gautammishrakashyap <kumargautam7071@gmail.com>
2024-12-15 14:22:12 +0530

    two

---
+++ day_2/two.java
@@ -0,0 +1,16 @@
+// Write a program to find the maximum of two numbers using an if-else statement.
+public class two {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        if (a>b) {
+            System.out.println(a);
+            
+        }
+        else{
+            System.out.println(b);
+        }
+        
+    }
+    
+}
