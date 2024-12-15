b3340e3d4fe799b652e97c0db8a0e8a7beb35e67
gautammishrakashyap <kumargautam7071@gmail.com>
2024-12-15 14:42:48 +0530

    three

---
+++ day_2/three.java
@@ -0,0 +1,20 @@
+// Write a program to calculate the area of a rectangle.
+// Input the length and width from the user.
+import java.util.Scanner; 
+
+
+public class three {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter length");
+        int len = sc.nextInt();
+
+        System.out.println("enter width");
+        int width = sc.nextInt();
+
+        System.out.println(len*width);
+
+        
+    }
+}
