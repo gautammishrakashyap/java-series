e4bdb542232c9f3e3811be9975c3e72b84149c36
gautammishrakashyap <kumargautam7071@gmail.com>
2024-12-15 14:17:54 +0530

    one

--- day_2/star.java
+++ day_2/star.java
@@ -1,5 +1,14 @@
+// # Write a program to check if a given number is even or odd.
 public class star {
    public static void main(String[] args) {
-    System.out.println("hello");
+    int num = 3;
+    if (num%2==0) {
+        System.out.println("even");
+
+        
+    }
+    else{
+        System.out.println("odd");
+    }
    }
 }
