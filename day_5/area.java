767bc02c14341d17ec82d1bf40e180903773dfd3
gautammishrakashyap <kumargautam7071@gmail.com>
2025-01-13 15:27:10 +0530

    new

---
+++ day_5/area.java
@@ -0,0 +1,21 @@
+// Write a program to calculate the area of a rectangle.
+
+// Input: Length and breadth of the rectangle.
+// Output: Print the calculated area.
+// Formula: 
+// Area
+// =
+// length
+// ×
+// breadth
+// Area=length×breadth.
+public class area {
+    public static void main(String[] args) {
+        int len = 30;
+        int breadth = 3;
+        System.out.println(len*breadth);
+        
+    }
+    
+
+}
---
+++ day_5/cir.java
@@ -0,0 +1,28 @@
+// (Easy) Write a program to calculate the perimeter of a circle.
+
+// Input: Radius of the circle.
+// Output: Print the calculated perimeter.
+// Formula: 
+// Perimeter
+// =
+// 2
+// ×
+// 𝜋
+// ×
+// radius
+// Perimeter=2×π×radius.
+// Use 
+// 𝜋
+// =
+// 3.14159
+// π=3.14159.
+public class cir {
+    public static void main(String[] args) {
+        double pi = 3.1;
+        int radius = 3;
+        System.out.println(2*radius*pi);
+
+        
+    }
+    
+}
