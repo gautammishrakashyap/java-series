fcd996706a393381cf41e6343979b1b480202706
gautammishrakashyap <kumargautam7071@gmail.com>
2025-01-24 14:08:14 +0530

    day 8

---
+++ day_8/even.java
@@ -0,0 +1,17 @@
+// (Easy) Write a program to print all even numbers between 1 and a user-given number n using a while loop.
+// Input: n = 10
+public class even {
+    public static void main(String[] args) {
+        int num = 0;
+        while (num<=10 ) {
+            if (num%2==0) {
+                
+                System.out.println(num);
+            }
+            num++;
+
+            
+        }
+    }
+    
+}
---
+++ day_8/pri.java
@@ -0,0 +1,12 @@
+public class pri {
+    public static void main(String[] args) {
+        int num= 0;
+        while (num<11) {
+            num++;
+            
+            System.out.println(num);
+        }
+        
+    }
+    
+}
