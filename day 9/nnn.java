6e433bd99f96bafc3582dddc4586ce070dfdab4e
gautammishrakashyap <kumargautam7071@gmail.com>
2025-01-26 13:48:13 +0530

    done

---
+++ day 9/nnn.java
@@ -0,0 +1,13 @@
+// (Very Easy) Write a program to print numbers from 1 to 20 using a while loop.
+
+// Output: 1, 2, 3, ..., 20
+package day 9;
+
+public class nnn {
+    public static void main(String[] args) {
+        int num = 0;
+        
+    }
+
+    
+}
